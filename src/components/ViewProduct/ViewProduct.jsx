3a9f32275a43c867ac39f25a2dc28f585bb26ebb
Jesús <jesuscarrillogonzalez.93@gmail.com>
2024-03-07 21:03:39 +0100

    Modify ViexProduct
    -Modify component ViewProduct adding function to swap between images, information.
    -Add css for ViewProduct component

--- src/components/ViewProduct/ViewProduct.jsx
+++ src/components/ViewProduct/ViewProduct.jsx
@@ -5,6 +5,7 @@ import './ViewProduct.css';
 
 function ViewProduct({ figureId }) {
   const [figureData, setFigureData] = useState(null);
+  const [currentImage, setCurrentImage] = useState('');
   const [isLoading, setIsLoading] = useState(true);
   const [isLoginModalOpen, setIsLoginModalOpen] = useState(false);
   const discount = 0.8;
@@ -16,6 +17,7 @@ function ViewProduct({ figureId }) {
         const response = await fetch(`http://localhost:7000/v1/figures/${figureId}`);
         const data = await response.json();
         setFigureData(data);
+        setCurrentImage(data.principalImage);
         setIsLoading(false);
       } catch (error) {
         console.error("Error fetching figure data:", error);
@@ -25,6 +27,10 @@ function ViewProduct({ figureId }) {
 
     fetchData();
   }, [figureId]);
+
+  const handleImageClick = (img) => {
+    setCurrentImage(img);
+  };
 
   const addToCart = async () => {
     const token = localStorage.getItem('token');
@@ -61,7 +67,7 @@ function ViewProduct({ figureId }) {
         alert("Ya tienes esta figura en el carrito, solo puedes añadirla una vez");
       }
     } catch (error) {
-      console.error("Error al añadir al carrito:", error);
+      console.error("Error: ", error);
       alert("Ha habido un error al añadir la figura al carrito. Inténtelo de nuevo más tarde.")
     }
   };
@@ -70,22 +76,45 @@ function ViewProduct({ figureId }) {
     return <Loader />;
   }
 
-  const { name, character, company, price, dimensions, material, brand, principalImage, amount } = figureData;
+  const { name, character, company, price, dimensions, material, brand, amount } = figureData;
 
   return (
     <div className="view-container">
-      <img className="view-image" src={`/resources/figures/${principalImage}`} alt={name} />
-      <div className="view-title">{name}</div>
-      <div className="view-character">Personaje: {character}</div>
-      <div className="view-company">Franquicia: {company}</div>
-      <div className="view-price">Precio: {(price*discount).toFixed(2)}€ <span>(PVP: {price}€)</span></div>
-      <div className="view-dimensions">Dimensiones: {dimensions}</div>
-      <div className="view-material">Materiales: {material}</div>
-      <div className="view-brand">Marca: {brand}</div>
-      <div className="view-amount">Existencias: {amount}</div>
-      <button className="addToCart" onClick={addToCart}>
-        <span>Añadir al carrito</span>
-      </button>
+      <div>
+        <img className="view-image" src={`/resources/figures/${currentImage}`} alt={name} />
+        <div className="image-thumbnails">
+          {[figureData.principalImage, ...figureData.images].map((img, index) => (
+            <img
+              key={index}
+              src={`/resources/figures/${img}`}
+              alt={`Thumbnail ${index}`}
+              onClick={() => handleImageClick(img)}
+              className={currentImage === img ? 'active-thumbnail' : ''}
+            />
+          ))}
+        </div>
+      </div>
+      <div>
+        <div className="view-title">{name}</div>
+        <div className="view-character">Figura de {character}, personaje del manga y anime {company}</div>
+        <div className="view-brand">Marca: {brand}</div>
+        <div className="view-company">Franquicia: {company}</div>
+        <div className="view-character-name">Nombre de personaje: {character}</div>
+        <div className="view-material">Materiales: {material}</div>
+        <div className="view-dimensions">Dimensiones: {dimensions}</div>
+        <div className="view-amount">Existencias: {amount}</div>
+        <div className="price-button-container">
+          <div className="view-price">
+            <div className="prices-info">
+              <span className="original-price">{price}€</span>
+              <span className="new-price">{(price*discount).toFixed(2)}€</span>
+            </div>
+          </div>
+          <button className="addToCart" onClick={addToCart}>
+            <span>Añadir al carrito</span>
+          </button>
+        </div>
+      </div>
       {isLoginModalOpen && <LoginModal onClose={() => setIsLoginModalOpen(false)} />}
     </div>
   );
