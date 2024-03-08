7a826622a7d925041f1708811fdf09f6eeb1cab3
Jesus Carrillo Gonzalez <121667616+JCarGon@users.noreply.github.com>
2024-03-08 18:56:17 +0100

    Add styles for user-icon when user is logged

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,11 @@
 import React, { useState } from "react";
-import { useNavigate } from 'react-router-dom';
-import { Link } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import search from "../../images/search.webp";
 import user from "../../images/user.svg";
 import cart from "../../images/cart.svg";
 import LoginModal from "../Login/LoginModal";
-import RegisterModal from '../Register/RegisterModal';
+import RegisterModal from "../Register/RegisterModal";
 import "./Header.css";
 
 function Header() {
@@ -16,9 +16,9 @@ function Header() {
   const baseUrl = process.env.REACT_APP_BASE_URL;
   const navigate = useNavigate();
 
-  const username = localStorage.getItem('username');
+  const username = localStorage.getItem("username");
   let cartCount = 0;
-  const userCartString = localStorage.getItem('userCart');
+  const userCartString = localStorage.getItem("userCart");
   if (userCartString) {
     try {
       const cartItems = JSON.parse(userCartString);
@@ -27,9 +27,9 @@ function Header() {
       console.error("Error al parsear userCart", error);
     }
   }
-  
+
   const handleUserIconClick = async () => {
-    const token = localStorage.getItem('token');
+    const token = localStorage.getItem("token");
     if (!token) {
       alert("Inicie sesión para acceder al perfil de usuario.");
       setIsLoginModalOpen(true);
@@ -43,13 +43,13 @@ function Header() {
       const response = await fetch(`${baseUrl}/v1/logout`, {
         method: "POST",
         headers: {
-          "Authorization": `Bearer ${localStorage.getItem('token')}`,
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
         },
       });
       if (response.ok || response.status === 401) {
-        localStorage.removeItem('token');
-        localStorage.removeItem('username');
-        localStorage.removeItem('userCart');
+        localStorage.removeItem("token");
+        localStorage.removeItem("username");
+        localStorage.removeItem("userCart");
         alert("Sesión cerrada correctamente. Volviendo a la página de inicio.");
         navigate("/");
       }
@@ -76,9 +76,9 @@ function Header() {
   };
 
   const handleCartClick = () => {
-    const token = localStorage.getItem('token');
+    const token = localStorage.getItem("token");
     if (token) {
-      navigate('/cart');
+      navigate("/cart");
     } else {
       alert("Inicie sesión para acceder al carrito.");
       setIsLoginModalOpen(true);
@@ -87,10 +87,12 @@ function Header() {
 
   return (
     <div className="header-container">
-      <Link to="/"><p className="nombreEmpresa">FRIKILEVEL</p></Link>
+      <Link to="/">
+        <p className="nombreEmpresa">FRIKILEVEL</p>
+      </Link>
       <div className="search-bar">
-      <input 
-          type="text" 
+        <input
+          type="text"
           placeholder="Buscar una figura por nombre..."
           value={searchText}
           onChange={(e) => setSearchText(e.target.value)}
@@ -100,7 +102,11 @@ function Header() {
         </button>
       </div>
       <div className="user-icon" onClick={handleUserIconClick}>
-        <img src={user} alt="icono inicio de sesión" />
+        <img
+          src={user}
+          alt="icono inicio de sesión"
+          className={username ? "user-img-logged-in" : "user-img"}
+        />
         {username && <div className="username-welcome">Hola, {username}</div>}
         {showDropdown && (
           <div className="user-dropdown">
@@ -113,8 +119,15 @@ function Header() {
         <img src={cart} alt="icono de carrito" />
         {cartCount > 0 && <div className="cart-counter">{cartCount}</div>}
       </div>
-      {isLoginModalOpen && <LoginModal onClose={handleLoginModalClose} onRegisterClick={handleRegisterModalOpen} />}
-      {isRegisterModalOpen && <RegisterModal onClose={handleRegisterModalClose} />}
+      {isLoginModalOpen && (
+        <LoginModal
+          onClose={handleLoginModalClose}
+          onRegisterClick={handleRegisterModalOpen}
+        />
+      )}
+      {isRegisterModalOpen && (
+        <RegisterModal onClose={handleRegisterModalClose} />
+      )}
     </div>
   );
 }
